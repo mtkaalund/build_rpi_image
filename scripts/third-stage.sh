a27d7f823134f3fd823357d39de78b9adeb6cbe6
Michael Torp Kaalund <kaalund@gmail.com>
2016-10-02 22:33:30 +0200

    removed deleting script from third-stage.sh

--- scripts/third-stage.sh
+++ scripts/third-stage.sh
@@ -6,7 +6,11 @@ apt -y install git-core binutils ca-certificates rpi-update
 touch /boot/start.elf
 rpi-update
 apt -y install locales console-common ntp openssh-server less vim
+#goes into scripts
+cd /scripts
+./run_external.sh
+cd
+
 echo "root:raspberry" | chpasswd
 sed -i -e 's/KERNEL\!=\"eth\*|/KERNEL\!=\"/' /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -f /etc/udev/rules.d/70-persistent-net.rules
-rm -f third-stage
